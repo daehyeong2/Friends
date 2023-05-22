80cf6f8af24b74002f9766cb6d88d556dec6a7fb
daehyeong2 <96002493+daehyeong2@users.noreply.github.com>
2023-05-22 11:45:15 +0900

    Night Mode Create and UI Update
    고라니 소개 페이지와 친구 소개 페이지의 UI를 대폭 개선하였고 나이트 모드를 추가했다.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const notification = document.querySelector(".notification");
+const btn = document.querySelector(".nav button");
+let nightMode = false;
 
 function handelNotificationRemove(){
     notification.style.transform = "translateY(200px)";
@@ -6,5 +8,18 @@ function handelNotificationRemove(){
         notification.parentNode.removeChild(notification);
     }, 700)
 }
+
+function handelNightModeClick(){
+    if(nightMode){
+        document.body.style.backgroundColor = "rgba(36, 154, 148, 0.1)";
+        nightMode = false;
+    }
+    else{
+        document.body.style.backgroundColor = "rgba(0,0,0,0.2";
+        nightMode = true;
+    }
+}
+
+btn.addEventListener("click", handelNightModeClick);
 
 notification.addEventListener("click", handelNotificationRemove);
