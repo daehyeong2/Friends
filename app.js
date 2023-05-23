14c676b46cf072f9e98960be6133c25f460f5bae
daehyeong2 <96002493+daehyeong2@users.noreply.github.com>
2023-05-23 16:31:54 +0900

    애니메이션 개선

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const notification = document.querySelector(".notification");
 const btn = document.querySelector(".nav button");
 const info = document.querySelector(".introduction-left__info");
 const infoItems = document.querySelectorAll(".introduction-left__info span");
+const tmp = infoItems[2].innerText;
 let nightMode = false;
 
 function handelNotificationRemove(){
@@ -29,16 +30,23 @@ if (info) {
 
 function handelInfoMouseEnter(){
     info.style.transform = "rotateY(180deg)";
+    info.style.backgroundColor = "lightblue";
+    infoItems[2].style.transform = "rotateY(180deg)";
     infoItems.forEach(infoItem => {
         infoItem.style.opacity = '0';
     });
+    infoItems[2].innerText = "정보";
+    infoItems[2].style.opacity = '1';
 }
 
 function handelInfoMouseLeave(){
     info.style.transform = "none";
+    info.style.backgroundColor = "rgba(0,0,0,0.2)";
     infoItems.forEach(infoItem => {
         infoItem.style.opacity = '1';
     });
+    infoItems[2].style.transform = "none";
+    infoItems[2].innerText = tmp;
 }
 
 btn.addEventListener("click", handelNightModeClick);
