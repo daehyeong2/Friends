1cfccc017fb51077f3c2aca01713d05e9e5f2afc
daehyeong2 <96002493+daehyeong2@users.noreply.github.com>
2023-06-01 11:30:56 +0900

    nightmode 최적화
    롤백

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const btn = document.querySelector(".nav button");
 const info = document.querySelector(".introduction-left__info");
 const infoItems = document.querySelectorAll(".introduction-left__info span");
 const tmp = infoItems[2].innerText;
+let Nightmode = false
 
 function handelNightModeClick(){
-    if(document.body.style.backgroundColor==="rgba(36, 154, 148, 0.1)"){
-        document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
-    }
-    else{
+    if(Nightmode){
         document.body.style.backgroundColor = "rgba(36, 154, 148, 0.1)";
+        Nightmode=false;
+    } else {
+        document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
+        Nightmode=true;
     }
 }
 
