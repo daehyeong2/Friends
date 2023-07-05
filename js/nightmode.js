0fa2c66894707ffe2648ddb172ab9993b4e8e884
daehyeong2 <daehyeong2010@naver.com>
2023-07-05 20:30:03 +0900

    debug console log code delete
    console.log(true/false);

--- js/nightmode.js
+++ js/nightmode.js
@@ -4,10 +4,8 @@ const night = localStorage.getItem(NIGHTMODE_KEY);
 function handelNightModeClick(){
     if(localStorage.getItem(NIGHTMODE_KEY)==="on"){
         localStorage.setItem(NIGHTMODE_KEY, "off");
-        console.log(true);
     } else {
         localStorage.setItem(NIGHTMODE_KEY, "on");
-        console.log(false);
     }
     document.body.classList.toggle("nightMode");
 }
